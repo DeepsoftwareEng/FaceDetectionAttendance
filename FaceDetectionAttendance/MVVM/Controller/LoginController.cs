4bb00ca4687ae31069d7837524fb01df91cc874d
DeepsoftwareEng <Deepit2507@gmail.com>
2023-04-25 15:58:35 +0700

    Change back to MVVM

---
+++ FaceDetectionAttendance/MVVM/Controller/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using FaceDetectionAttendance.MVC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceDetectionAttendance.MVC.Controller
+{
+    public class LoginController
+    {
+        private readonly AccountModel accountModel;
+
+        public LoginController(AccountModel accountModel)
+        {
+            this.accountModel = accountModel;
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            return username == accountModel.username && password == accountModel.password;
+        }
+    }
+}
