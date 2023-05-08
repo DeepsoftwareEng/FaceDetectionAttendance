988a6f4cf3b42dd291a31d251595ad2032c2ce4b
hung nguyen <minhvt223@gmail.com>
2023-05-08 08:58:06 +0700

    adjust attendance

--- FaceDetectionAttendance/MVVM/View/AttendanceUI.xaml.cs
+++ FaceDetectionAttendance/MVVM/View/AttendanceUI.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Media;
 using Emgu.CV.CvEnum;
 using System.Windows.Media.Imaging;
 using System.Threading.Tasks;
-
+using System.Drawing;
+using Emgu.CV.Face;
 
 namespace FaceDetectionAttendance.MVVM.View
 {
@@ -99,12 +100,93 @@ namespace FaceDetectionAttendance.MVVM.View
         private void StartCam_Click(object sender, EventArgs e)
         {
             _videoSource = new VideoCapture();
-            //_processingTask = Task.Run(() => ProcessFramesAsync());
+            _processingTask = Task.Run(() => ProcessFramesAsync());
         }
         private void StopCam_Click(object sender, EventArgs e)
         {
             _videoSource.Stop();
         }
-       
+       private async Task ProcessFramesAsync()
+       {
+           while(_videoSource.IsOpened)
+            {
+                var frame = _videoSource.QueryFrame().ToImage<Bgr, byte>();
+                if(frame == null)
+                {
+                    break;
+                }
+                var faces = DetectFaces(frame);
+                foreach( var face in faces)
+                {
+                    var roi = frame.Copy(face);
+                    var grayRoi = roi.Convert<Gray, byte>().Resize(100, 100, Inter.Cubic);
+                    var result = RecognizeWorker(grayRoi);
+                    if (result.Label != -1 && result.Distance < 3000)
+                    {
+                        await Dispatcher.InvokeAsync(() =>
+                        {
+                            // display the recognized worker name on the UI thread
+                           
+                        });
+                    }
+
+                    var rect = new Rectangle(face.X, face.Y, face.Width, face.Height);
+                    var highlightRect = new Rectangle((int)(rect.X * VideoDisplay.ActualWidth / frame.Width),
+                                                       (int)(rect.Y * VideoDisplay.ActualHeight / frame.Height),
+                                                       (int)(rect.Width * VideoDisplay.ActualWidth / frame.Width),
+                                                       (int)(rect.Height * VideoDisplay.ActualHeight / frame.Height));
+
+                // display the frame on the UI thread
+                var imageSource = BitmapSourceConvert.ToBitmapSource(frame);
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    //CameraGrid.Background = new ImageBrush(imageSource);
+                });
+            }
+            }
+       }
+        private Rectangle[] DetectFaces(Image<Bgr, byte> image)
+        {
+            using var gray = image.Convert<Gray, byte>();
+            var faces = _faceClassifier.DetectMultiScale(gray,1.1,3,new System.Drawing.Size(500,500));
+            return faces;
+        }
+
+        private (int Label, double Distance) RecognizeWorker(Image<Gray, byte> image)
+        {
+            var recognizer = new EigenFaceRecognizer(WorkerList.Count, double.PositiveInfinity);
+            var labels = new List<int>();
+            for (var i = 0; i < WorkerList.Count; i++)
+            {
+                labels.Add(i);
+            }
+            recognizer.Train(WorkerList.ToArray(), labels.ToArray());
+
+            var result = recognizer.Predict(image);
+            return (result.Label, result.Distance);
+        }
     }
+    public static class BitmapSourceConvert
+        {
+            [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+            private static extern bool DeleteObject(System.IntPtr hObject);
+
+            public static BitmapSource ToBitmapSource<T>(this Image<T, byte> image) where T : struct, IColor
+            {
+                using var bitmap = image.ToBitmap();
+                var hBitmap = bitmap.GetHbitmap();
+                try
+                {
+                    return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                        hBitmap,
+                        System.IntPtr.Zero,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                }
+                finally
+                {
+                    DeleteObject(hBitmap);
+                }
+            }
+        }
 }
