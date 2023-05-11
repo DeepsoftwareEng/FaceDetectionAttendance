6b20cd2b687da47e79cf6c5e70823a82be4923fa
GuenKainto <khanhtho10122002@gmail.com>
2023-05-11 22:22:31 +0700

    need to add package OpenXML , 4G so lag :"((( i need wifi

--- FaceDetectionAttendance/MVVM/View/ReportUI.xaml.cs
+++ FaceDetectionAttendance/MVVM/View/ReportUI.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Emgu.CV.CvEnum;
 using FaceDetectionAttendance.MVVM.Model;
 using Microsoft.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel; //Alias : công cụ giúp đặt tên khác cho 1 kiểu dư liệu
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -268,5 +269,31 @@ namespace FaceDetectionAttendance.MVVM.View
             }
         }
 
+        private void Export_Button_Click(object sender, RoutedEventArgs e)
+        {
+            // create an instance of Excel application
+            Excel.Application excelApp = new Excel.Application();
+
+            // make Excel application visible (optional)
+            excelApp.Visible = true;
+
+            // create a new Workbook
+            Excel.Workbook newWorkbook = excelApp.Workbooks.Add();
+
+            // create a new Worksheet
+            Excel.Worksheet newWorksheet = newWorkbook.Sheets.Add();
+
+            // enter data into Worksheet cells
+            newWorksheet.Cells[1, 1] = "Header 1";
+            newWorksheet.Cells[2, 1] = "Data 1";
+            newWorksheet.Cells[3, 1] = "Data 2";
+
+            // save Workbook 
+            newWorkbook.SaveAs(@"D:\test.xlsx");
+
+            // close Workbook and Excel application
+            newWorkbook.Close();
+            excelApp.Quit();
+        }
     }
 }
