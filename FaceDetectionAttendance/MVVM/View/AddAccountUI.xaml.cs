61d343776b2dca2774fd3204c069d661a6482092
vietanh1110 <vietanhdang123987@gmail.com>
2023-05-14 20:23:15 +0700

    Viet Anh update Back_button in AddAccount

--- FaceDetectionAttendance/MVVM/View/AddAccountUI.xaml.cs
+++ FaceDetectionAttendance/MVVM/View/AddAccountUI.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using FaceDetectionAttendance.MVVM.Model;
+using Unity.Policy;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace FaceDetectionAttendance.MVVM.View
 {
@@ -20,6 +24,8 @@ namespace FaceDetectionAttendance.MVVM.View
     /// </summary>
     public partial class AddAccountUI : Page
     {
+        private Dataconnecttion dtc = new Dataconnecttion();
+        private SqlCommand SQLcommand;
         public AddAccountUI()
         {
             InitializeComponent();
@@ -28,6 +34,36 @@ namespace FaceDetectionAttendance.MVVM.View
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Addimagebtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Add_SetComboBoxData()
+        {
+
+            string querry = "Select* from Faculty";
+            if (dtc.GetConnection().State == System.Data.ConnectionState.Closed)
+                dtc.GetConnection().Open();
+            SQLcommand = new SqlCommand(querry, dtc.GetConnection());
+            SqlDataReader reader = SQLcommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Faculty a = new Faculty();
+                a.IdFaculty = reader.GetString(0);
+                a.NameFaculty = reader.GetString(1);
+                facultycbb.Items.Add(a.IdFaculty);
+            }
+            Rolecbb.Items.Add("Admin");
+            Rolecbb.Items.Add("Staff");
+        }
+
+
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
         }
     }
 }
