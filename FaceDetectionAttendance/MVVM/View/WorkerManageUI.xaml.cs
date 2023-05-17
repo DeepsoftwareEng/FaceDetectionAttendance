ae3d3bb9db39dcd91e972d9d5e203b600d3c6e12
emhungg <ccc2932002@gmail.com>
2023-05-17 20:36:44 +0700

    AddWorkerUI.xaml

--- FaceDetectionAttendance/MVVM/View/WorkerManageUI.xaml.cs
+++ FaceDetectionAttendance/MVVM/View/WorkerManageUI.xaml.cs
@@ -24,5 +24,10 @@ namespace FaceDetectionAttendance.MVVM.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
