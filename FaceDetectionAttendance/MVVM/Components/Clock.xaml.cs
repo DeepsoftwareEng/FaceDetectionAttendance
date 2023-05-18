473dcc8ca5aeebe703d2d8a963435812d99feb82
DeepsoftwareEng <Deepit2507@gmail.com>
2023-05-18 16:48:51 +0700

    add clock

---
+++ FaceDetectionAttendance/MVVM/Components/Clock.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FaceDetectionAttendance.MVVM.Components
+{
+    /// <summary>
+    /// Interaction logic for Clock.xaml
+    /// </summary>
+    public partial class Clock : UserControl
+    {
+        private DispatcherTimer timer;
+
+        public Clock()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
