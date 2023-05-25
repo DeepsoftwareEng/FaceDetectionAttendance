228d226e822487148348f49c5d6e2d60402808a6
vietanh1110 <vietanhdang123987@gmail.com>
2023-05-25 19:28:30 +0700

    finish process request module

--- FaceDetectionAttendance/MVVM/View/RequestUI.xaml.cs
+++ FaceDetectionAttendance/MVVM/View/RequestUI.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using FaceDetectionAttendance.MVVM.Model;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Emgu.CV.XImgproc;
 
 namespace FaceDetectionAttendance.MVVM.View
 {
@@ -20,9 +14,104 @@ namespace FaceDetectionAttendance.MVVM.View
     /// </summary>
     public partial class RequestUI : Page
     {
+        Dataconnecttion dtc = new Dataconnecttion();
+        SqlCommand cmd = new SqlCommand();
+        List<Request> requests = new List<Request>();
         public RequestUI()
         {
             InitializeComponent();
+            setStatusData();
+            setData();
+        }
+        private void setStatusData()
+        {
+            Statuscbb.Items.Add("Idle");
+            Statuscbb.Items.Add("Accepted");
+            Statuscbb.Items.Add("Denied");
+        }
+        private void setData()
+        {
+            string query = "select * from Request";
+            if (dtc.GetConnection().State == System.Data.ConnectionState.Closed)
+            {
+                dtc.GetConnection().Open();
+            }
+            try
+            {
+                cmd = new SqlCommand(query, dtc.GetConnection());
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Request request = new Request();
+                    request.id = reader.GetInt32(0);
+                    request.id_attendance = reader.GetInt32(1);
+                    request.detail = reader.GetString(2);
+                    request.states = reader.GetString(3);
+                    request.usernamesent = reader.GetString(4);
+                    requests.Add(request);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Acceptbtn_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = Requestdtg.SelectedItem;
+            int id = 0;
+            if (temp != null)
+            {
+                dynamic selected = temp;
+                id = selected.id;
+            }
+            string query = "Update Request set states = 'Accept' where id = @id";
+            try
+            {
+                cmd = new SqlCommand(query, dtc.GetConnection());
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            dtc.GetConnection().Close();
+        }
+
+        private void Denybtn_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = Requestdtg.SelectedItem;
+            int id = 0;
+            if (temp != null)
+            {
+                dynamic selected = temp;
+                id = selected.id;
+            }
+            string query = "Update Request set states = 'Deny' where id = @id";
+            try
+            {
+                cmd = new SqlCommand(query, dtc.GetConnection());
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            dtc.GetConnection().Close();
+        }
+
+        private void Statuscbb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Statuscbb.SelectedItem != null)
+            {
+                var selectedItem = from request in requests where request.states == Statuscbb.SelectedItem.ToString() select request;
+                if (selectedItem != null)
+                {
+                    Requestdtg.ItemsSource = selectedItem;
+                }
+            }
         }
     }
 }
