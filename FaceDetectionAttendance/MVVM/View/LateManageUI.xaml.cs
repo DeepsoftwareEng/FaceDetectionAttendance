6c576d79b892c4405fa6c574c302508fe20a4816
Anhquan15323 <127971295+Anhquan15323@users.noreply.github.com>
2023-05-25 21:26:39 +0700

    seach funtion

--- FaceDetectionAttendance/MVVM/View/LateManageUI.xaml.cs
+++ FaceDetectionAttendance/MVVM/View/LateManageUI.xaml.cs
@@ -64,6 +64,12 @@ namespace FaceDetectionAttendance.MVVM.View
             InitializeComponent();
             this.faculty = faculty;
             Faculty_Header.Text = faculty;
+            for (int i = 1; i <= 12; i++)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = i;
+                Month_ComboBox.Items.Add(item);
+            }
             setLate_DataGrid();
         }
 
@@ -157,6 +163,66 @@ namespace FaceDetectionAttendance.MVVM.View
                 }
             }
             else setLate_DataGrid();
+        }
+
+        private void Search_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Search_TextBox.Text.Trim() != "" &&
+                Month_ComboBox.SelectedItem != null &&
+                Year_TextBox.Text.Trim() != "")
+            {
+                string name = Search_TextBox.Text.Trim();
+                int month = Convert.ToInt32(Month_ComboBox.SelectedValue);
+                int year = Int32.Parse(Year_TextBox.Text.Trim());
+                try
+                {
+                    Late_DataGrid.Items.Clear();
+                    string querry = "SELECT LateList.id, LateList.id_worker, " +
+                                    "fullname, d_m, shift_worked, detail " +
+                                    "FROM LateList INNER JOIN WorkerList ON " +
+                                    "LateList.id_worker = WorkerList.id " +
+                                    "AND id_faculty = @faculty " +
+                                    "WHERE fullname = @fullname " +
+                                    "AND MONTH(d_m) = @month " +
+                                    "AND YEAR(d_m) = @year ";
+                    if (dtc.GetConnection().State == System.Data.ConnectionState.Closed)
+                    {
+                        dtc.GetConnection().Open();
+                    }
+                    cmd = new SqlCommand(querry, dtc.GetConnection());
+                    cmd.Parameters.AddWithValue("@faculty", this.faculty);
+                    cmd.Parameters.AddWithValue("@fullname", name);
+                    cmd.Parameters.AddWithValue("@month", month);
+                    cmd.Parameters.AddWithValue("@year", year);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        int id_worker = reader.GetInt32(1);
+                        string name_worker = reader.GetString(2);
+                        DateTime date = reader.GetDateTime(3);
+                        int shift = reader.GetInt32(4);
+                        string detail = reader.GetString(5);
+                        Late_DataGrid.Items.Add(new { Id = id, IdWorker = id_worker, NameWorker = name_worker, DateTime = date.ToString(), Shift = shift, Detail = detail });
+                    }
+                    reader.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error");
+                }
+            }
+            else MessageBox.Show("Please enter full date in text box", "Message");
+        }
+
+        private void Year_TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1))
+            {
+                e.Handled = true;
+                Message_Label.Visibility = Visibility.Visible;
+            }
         }
     }
 }
