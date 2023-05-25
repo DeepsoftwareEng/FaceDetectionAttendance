e4588ae979d4c06c4e9a4fd40fe279bba4d6aa86
DeepsoftwareEng <Deepit2507@gmail.com>
2023-05-25 18:10:07 +0700

    Add model Request

--- FaceDetectionAttendance/MVVM/Model/Request.cs
+++ FaceDetectionAttendance/MVVM/Model/Request.cs
@@ -8,5 +8,10 @@ namespace FaceDetectionAttendance.MVVM.Model
 {
     class Request
     {
+        public int id { get; set; }
+        public int id_attendance { get; set; }
+        public string detail { get; set; }
+        public string states { get; set; }
+        public string usernamesent { get; set; }
     }
 }
